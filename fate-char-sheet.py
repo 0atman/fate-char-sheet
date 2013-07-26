3ec2a30c829c5208ea86294f1fbe26d18f1d2781
Tristram Oaten <tristram@oaten.name>
2013-07-26 13:44:33 +0100

    read-only API

--- fate-char-sheet.py
+++ fate-char-sheet.py
@@ -1,14 +1,13 @@
 import os
 
-from flask import Flask
+from flask import Flask, render_template
 from flask.ext import admin
-from flask import render_template
 from flask.ext.admin.contrib.mongoengine import ModelView
 from flask.ext.mongoengine import MongoEngine
 from mongoengine import connect
+from flask.ext.restful import Resource, Api
 
-from models import User, Todo, Tag, Post
-from admin import UserView
+from models import Character
 
 # Create application
 app = Flask(__name__)
@@ -32,19 +31,29 @@ db.init_app(app)
 # Flask views
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', character="test")
+
+
+class CharacterResource(Resource):
+    def get(self, name):
+        char = Character.objects.get_or_404(name=name).to_mongo()
+        del char['_id']
+        del char['_cls']
+        del char['_types']
+        return char
+
+api = Api(app)
+api.add_resource(CharacterResource, '/character/<string:name>')
 
 
 if __name__ == '__main__':
     # Create admin
-    admin = admin.Admin(app, 'Simple Models')
+    admin = admin.Admin(app, 'Fate Accelerated')
 
     # Add views
-    admin.add_view(UserView(User))
-    admin.add_view(ModelView(Todo))
-    admin.add_view(ModelView(Tag))
-    admin.add_view(ModelView(Post))
+    admin.add_view(ModelView(Character))
 
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
+    app.debug = True
     app.run(host='0.0.0.0', port=port)
--- models.py
+++ models.py
@@ -1,45 +1,18 @@
-import datetime
-
 from flask.ext.mongoengine import MongoEngine
 
 db = MongoEngine()
 
 
-class User(db.Document):
-    name = db.StringField(max_length=40)
-    tags = db.ListField(db.ReferenceField('Tag'))
-    password = db.StringField(max_length=40)
-
-    def __unicode__(self):
-        return self.name
-
-
-class Todo(db.Document):
-    title = db.StringField(max_length=60)
-    text = db.StringField()
-    done = db.BooleanField(default=False)
-    pub_date = db.DateTimeField(default=datetime.datetime.now)
-    user = db.ReferenceField(User)
-
-    # Required for administrative interface
-    def __unicode__(self):
-        return self.title
-
-
-class Tag(db.Document):
-    name = db.StringField(max_length=10)
-
-    def __unicode__(self):
-        return self.name
-
-
-class Comment(db.EmbeddedDocument):
-    name = db.StringField(max_length=20)
-    value = db.StringField(max_length=20)
-
-
-class Post(db.Document):
-    name = db.StringField(max_length=20)
-    value = db.StringField(max_length=20)
-    inner = db.ListField(db.EmbeddedDocumentField(Comment))
-    lols = db.ListField(db.StringField(max_length=20))
+class Character(db.Document):
+    name = db.StringField(max_length=60)
+    description = db.StringField()
+    high_concept = db.StringField(max_length=60)
+    trouble = db.StringField(max_length=60)
+    aspects = db.ListField(db.StringField(max_length=20))
+    stunts = db.ListField(db.StringField(max_length=20))
+    careful = db.IntField(min_value=0, max_value=3)
+    clever = db.IntField(min_value=0, max_value=3)
+    flashy = db.IntField(min_value=0, max_value=3)
+    forceful = db.IntField(min_value=0, max_value=3)
+    quick = db.IntField(min_value=0, max_value=3)
+    sneaky = db.IntField(min_value=0, max_value=3)
